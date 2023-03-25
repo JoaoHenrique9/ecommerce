345059efe2692e533d59cfc6b4b5e250f027433b
joaohenrique9 <joaomedeiros@cers.com.br>
2023-03-25 13:04:06 -0300

    criando product-service

---
+++ ec-product-service/src/main/java/com/example/ec/EcProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcProductServiceApplication.class, args);
+	}
+
+}
