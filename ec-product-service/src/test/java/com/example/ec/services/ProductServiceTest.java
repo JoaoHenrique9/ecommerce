6a48e95bb285a805d5e692fa76f5c06640c9cb10
joaoHenrique9 <joao.gh61@gmail.com>
2023-04-04 23:55:11 -0300

    criando test shouldFindAll. product-service.

--- ec-product-service/src/test/java/com/example/ec/services/ProductServiceTest.java
+++ ec-product-service/src/test/java/com/example/ec/services/ProductServiceTest.java
@@ -1,65 +1,105 @@
 package com.example.ec.services;
 
-import com.example.ec.exception.ObjectNotFoundException;
-import com.example.ec.models.ProductModel;
-import com.example.ec.repositories.ProductRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
-import java.util.Optional;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.when;
+import com.example.ec.exception.ObjectNotFoundException;
+import com.example.ec.models.ProductModel;
+import com.example.ec.repositories.ProductRepository;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
-    private static final UUID RANDOM_UUID = UUID.randomUUID();
-    private static final String JOHN_WICK = "John Wick";
+	private static final UUID RANDOM_UUID = UUID.randomUUID();
+	private static final String JOHN_WICK = "John Wick";
 
-    private ProductService productService;
+	private ProductService productService;
 
-    @Mock
-    private ProductRepository repository;
+	@Mock
+	private ProductRepository repository;
 
-    @BeforeEach
-    void setUp() {
-        productService = new ProductService(repository);
-    }
+	@BeforeEach
+	void setUp() {
+		productService = new ProductService(repository);
+	}
 
-    @Test
-    void shouldFindById() {
-        var id = RANDOM_UUID;
-        var expectedProductModel = createProductModel();
+	@Test
+	void shouldFindById() {
+		var id = RANDOM_UUID;
+		var expectedProductModel = createProductModel();
 
-        when(repository.findById(id)).thenReturn(Optional.of(expectedProductModel));
+		when(repository.findById(id)).thenReturn(Optional.of(expectedProductModel));
 
-        var actualProductModel = productService.findById(id);
+		var actualProductModel = productService.findById(id);
 
-        assertThat(actualProductModel).usingRecursiveComparison().isEqualTo(expectedProductModel);
-    }
+		assertThat(actualProductModel).usingRecursiveComparison().isEqualTo(expectedProductModel);
+	}
 
-    @Test
-    void shouldThrowObjectNotFoundExceptionWhenFindById() {
-        var id = RANDOM_UUID;
+	@Test
+	void shouldThrowObjectNotFoundExceptionWhenFindById() {
+		var id = RANDOM_UUID;
 
-        when(repository.findById(id)).thenReturn(Optional.empty());
+		when(repository.findById(id)).thenReturn(Optional.empty());
 
-        assertThatThrownBy(() -> productService.findById(id))
-                .isInstanceOf(ObjectNotFoundException.class)
-                .hasMessageContaining("Produto não encontrado");
+		assertThatThrownBy(() -> productService.findById(id))
+				.isInstanceOf(ObjectNotFoundException.class)
+				.hasMessageContaining("Produto não encontrado");
 
-    }
+	}
 
-    private static ProductModel createProductModel() {
-        var productModel = new ProductModel();
-        productModel.setId(RANDOM_UUID);
-        productModel.setName(JOHN_WICK);
-        return productModel;
-    }
+	@Test
+	void shouldFindAll() {
+		var expectedProductModelPageableList = createProductModelPageableList();
+		var pageable = createPageable();
+
+		when(repository.findAll(pageable)).thenReturn(expectedProductModelPageableList);
+
+		var actualProductModelPageableList = productService.findAll(pageable);
+
+		assertThat(actualProductModelPageableList).usingRecursiveComparison().isEqualTo(expectedProductModelPageableList);
+
+	}
+
+	private static ProductModel createProductModel() {
+		var productModel = new ProductModel();
+		productModel.setId(RANDOM_UUID);
+		productModel.setName(JOHN_WICK);
+		return productModel;
+	}
+
+	private static List<ProductModel> createProductModelList() {
+		var productModel = new ProductModel();
+		productModel.setId(RANDOM_UUID);
+		productModel.setName(JOHN_WICK);
+		return Arrays.asList(productModel);
+
+	}
+
+	private static Pageable createPageable() {
+		int page = 0;
+		int size = 10;
+		Sort sort = Sort.by("createdAt").descending();
+		return PageRequest.of(page, size, sort);
+	}
+
+	private Page<ProductModel> createProductModelPageableList() {
+		return new PageImpl<ProductModel>(createProductModelList());
+	}
 }
