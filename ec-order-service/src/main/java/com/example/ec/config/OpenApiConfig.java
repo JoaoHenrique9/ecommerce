4e773cd2743dd594420284bf1b9765b7b5446e32
joaoHenrique9 <joao.gh61@gmail.com>
2023-05-22 21:17:17 -0300

    config OpenAPI

---
+++ ec-order-service/src/main/java/com/example/ec/config/OpenApiConfig.java
@@ -0,0 +1,12 @@
+package com.example.ec.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(info = @Info(version = "1.0", title = "order-service"))
+@SecurityScheme(name = "bearerAuth", description = "JWT auth description", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
+public class OpenApiConfig {
+}
