aa7a12b8df51cd61c1188f2d89b273331b6e6ac3
joaoHenrique9 <joao.gh61@gmail.com>
2023-05-23 18:27:54 -0300

    criando eureka-server

---
+++ ec-eureka-server/src/main/java/com/example/ec/eceurekaserver/EcEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.ec.eceurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EcEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcEurekaServerApplication.class, args);
+	}
+
+}
---
+++ ec-eureka-server/src/test/java/com/example/ec/eceurekaserver/EcEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec.eceurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
