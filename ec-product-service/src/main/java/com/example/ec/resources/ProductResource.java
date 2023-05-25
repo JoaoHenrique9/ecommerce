5ecae10a305eeb7ca71db0996b38e16753c91325
joaoHenrique9 <joao.gh61@gmail.com>
2023-05-24 22:57:49 -0300

    refactoring product-service.

--- ec-product-service/src/main/java/com/example/ec/resources/ProductResource.java
+++ ec-product-service/src/main/java/com/example/ec/resources/ProductResource.java
@@ -12,12 +12,12 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -67,8 +67,8 @@ public class ProductResource {
 	}
 
 	@Hidden
-	@PatchMapping("/{id}/subtract-quantity")
-	public ResponseEntity<Void> subtractQuantity(@PathVariable UUID id, @NotEmpty @RequestBody Long quantity) {
+	@PutMapping("/{id}/subtract-quantity")
+	public ResponseEntity<Void> subtractQuantity(@PathVariable UUID id, @NotEmpty @RequestParam Long quantity) {
 		service.subtractQuantity(id, quantity);
 		return ResponseEntity.noContent().build();
 	}
