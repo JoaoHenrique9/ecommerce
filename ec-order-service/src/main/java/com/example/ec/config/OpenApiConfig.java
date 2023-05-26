c5cde866dd0d7cf5098c896f3fb3e684f96d2bf3
joaoHenrique9 <joao.gh61@gmail.com>
2023-05-26 19:23:31 -0300

    configurando OpenApi

--- ec-order-service/src/main/java/com/example/ec/config/OpenApiConfig.java
+++ ec-order-service/src/main/java/com/example/ec/config/OpenApiConfig.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-@OpenAPIDefinition(info = @Info(version = "1.0", title = "order-service"))
+@OpenAPIDefinition(info = @Info(version = "1.0", title = "order-service"), security = {
+        @SecurityRequirement(name = "bearerAuth")
+})
 @SecurityScheme(name = "bearerAuth", description = "JWT auth description", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
 public class OpenApiConfig {
 }
