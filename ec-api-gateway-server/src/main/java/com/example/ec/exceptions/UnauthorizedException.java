00ae4499d9844ae0e2563305ffaa0e04022f3f8e
joaoHenrique9 <joao.gh61@gmail.com>
2023-06-01 14:54:10 -0300

    retirando security da api-gateway

--- ec-api-gateway-server/src/main/java/com/example/ec/exceptions/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.example.ec.exceptions;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class UnauthorizedException extends AuthenticationException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
